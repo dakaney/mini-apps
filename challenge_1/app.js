dfc57121cf82dcd278eeb22621909973e6d32893
Dakaney <wayman_ng@yahoo.com>
2018-02-12 15:46:40 -0800

    added # of win table & previous winner goes first

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,8 +1,10 @@
 let count = 0;
+let winCountOne = 0;
+let winCountTwo = 0;
 let currentPlay = 'X'
 let array = [first, second, third, fourth, fifth, sixth, seventh, eigth, ninth];
 let board = [];
-
+let previousWinner = '';
 
 function changePlay() {
 	if(count === 9) {
@@ -16,7 +18,6 @@ function changePlay() {
 	}
 }
 
-
 function modifyText(event) {
     if (event.srcElement.textContent === 'X' || event.srcElement.textContent === 'O') {
       return;
@@ -26,100 +27,53 @@ function modifyText(event) {
     changePlay();
     board = [[first.textContent, second.textContent, third.textContent],
              [fourth.textContent, fifth.textContent, sixth.textContent],
-             [seventh.textContent, eigth.textContent, ninth.textContent]];
-    checkRow(2);
-    checkColumn(2);
-    checkDiagonal();
+             [seventh.textContent, eigth.textContent, ninth.textContent],
+             [first.textContent, fourth.textContent, seventh.textContent],
+             [second.textContent, fifth.textContent, eigth.textContent],
+             [third.textContent, sixth.textContent, ninth.textContent],
+             [first.textContent, fifth.textContent, ninth.textContent],
+             [third.textContent, fifth.textContent, seventh.textContent]];
+    checkWinner();
     }
 }
 
-function checkRow (row) {
-  	if(row === -1) {
-  		return;
-  	}
-  	var x = 0;
-  	var o = 0;
-    for (let i = 0; i < board.length; i++) {
-      if (board[row][i] === 'X'){
-      	x++;
-      } else if (board[row][i] === 'O') {
-      	o++;
-      }
-    }
-    if (x === 3) {
-      resetGame(null, 'X');
-  	  turnText.textContent = "Player one wins!!!"
-    }
-    if (o === 3) {
-      resetGame(null, 'O');
-      turnText.textContent = "Player two wins!!!"
-    }
-    checkRow(row-1);
+function checkWinner () {
+	board.forEach(array => {
+		var x = 0;
+		var o = 0;
+		array.forEach(square => {
+			if(square === 'X') {
+				x++;
+			}
+			if(square === 'O') {
+				o++
+			}
+		})
+		if(x === 3) {
+			resetGame(null, 'X');
+			turnText.textContent = "Player 1 wins!!!";
+			previousWinner = "Previous winner: Player 1";
+			winCountOne++;
+			winCounterOne.textContent = winCountOne + " wins";
+		}
+		if(o === 3) {
+			resetGame(null, 'O');
+			turnText.textContent = "Player 2 wins!!!";
+			previousWinner = "Previous winner: Player 2";
+			winCountTwo++;
+			winCounterTwo.textContent = winCountTwo + " wins";
+		}
+	})
 }
-
-function checkColumn (column) {
-	if(column === -1){
-		return;
-	}
-	var x = 0;
-	var o = 0;
-	for (let i = 0; i < board[column].length; i++) {
-      if (board[i][column] === 'X') {
-      	x++;
-      } else if (board[i][column] === 'O') {
-    	o++;
-      }
-    }
-    if (x === 3) {
-    	resetGame(null, 'X');
-    	turnText.textContent = "Player one wins!!!"
-    }
-    if (o === 3) {
-    	resetGame(null, 'O');
-    	turn.textContent = "Player two wins!!!"
-    }
-    checkColumn(column-1);
-}
-
-function checkDiagonal () {
-	if (board[0][0] === 'X'){
-		if(board[1][1] === 'X'){
-			if(board[2][2] === 'X'){
-				resetGame(null, 'X');
-    	        turnText.textContent = "Player one wins!!!"
-			}
-		}
-	}
-	if (board[0][0] === 'O'){
-		if(board[1][1] === 'O'){
-			if(board[2][2] === 'O'){
-				resetGame(null, 'O');
-    	        turnText.textContent = "Player two wins!!!"
-			}
-		}
-	}
-	if (board[0][2] === 'X'){
-		if(board[1][1] === 'X'){
-			if(board[2][0] === 'X'){
-				resetGame(null, 'X');
-    	        turnText.textContent = "Player one wins!!!"
-			}
-		}
-	}
-	if (board[0][2] === 'O'){
-		if(board[1][1] === 'O'){
-			if(board[2][0] === 'O'){
-				resetGame(null, 'O');
-    	        turnText.textContent = "Player two wins!!!"
-			}
-		}
-	}
-}
-
 
 function resetGame(event, change) {
     array.forEach(item => item.textContent = change);
-	count = 0;
+    previous.textContent = previousWinner;
+    if(previousWinner === "Previous winner: Player 2") {
+    	count = 1;
+    } else {
+	  count = 0;
+    }
 	changePlay();
 }
 array.forEach(item => item.addEventListener("click", modifyText, false));
