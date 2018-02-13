6a4c8ef793ec194c6ddc0fa3f10cc9a8c3024484
Dakaney <wayman_ng@yahoo.com>
2018-02-13 12:35:38 -0800

    got a working client, server and client connected

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,20 @@
+button.addEventListener('click', post);
 
+function post (e) {
+  e.preventDefault();
+  let message = textarea.value;
+  let callback = function (data) {
+    body.textContent = data;
+  }
+  $.ajax({
+  type: "POST",
+  url: '/',
+  contentType: "application/json",
+  dataType: "json",
+  data: message,
+  success: function(data){
+  	callback(JSON.stringify(data));
+  }
+  })
+}
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const parser = require('body-parser');
+const app = express()
 
 
+app.use(parser.json());
+app.use(express.static('client'))
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/', (req, res) => {
+	console.log(req.body);
+	res.send(req.body);
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
